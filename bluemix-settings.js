bdf2e99966cca600c48890203ab961916935b291
公彦 行武 <yukutake.tomohiko@gmail.com>
2017-03-23 21:16:16 -0500

    新規作成

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -49,7 +49,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+    	moment : require("moment")
+    },
 
     storageModule: require("./couchstorage")
 }
